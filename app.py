756bae015c28697c7a708e92e6a4dec0fd32ef02
KOTLIN <acarolinavilel@gmail.com>
2025-04-09 19:49:21 -0300

    feat: add turmas_bp and config.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from config import Config
 
 from Controllers.aluno_controller import aluno_bp
 from Controllers.professor_controller import professor_bp
+from Controllers.turma_controller import turma_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 app.register_blueprint(aluno_bp)
 app.register_blueprint(professor_bp)
+app.register_blueprint(turma_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 class Config:
     DEBUG = True
+    PORT = 5000
+    HOST = '127.0.0.1'
